5d3187256c93f932ca033fe118568d2def43e3c6
diminoend <diminuend@mail.ru>
2022-02-24 00:02:09 +0300

    3 урок. 4 юнит тест доделан

--- lesson3/homework3_var2.py
+++ lesson3/homework3_var2.py
@@ -36,21 +36,37 @@ class teacher(unittest.TestCase):
         self.assertEqual(a, b, 'Не найден якорь "Передайте доступы"')
 
     def test_03_add_student(self):
+        # В классе должен уже быть хотя бы 1 ученик
         wd = self.wd
-        # клик по Далее в онбординге
-        wd.find_element(By.XPATH, '//div[contains(text(),"Далее")]').click()
+        # ожидание и клик по Далее в онбординге
+        wdw(wd, 3).until(ec.presence_of_element_located((By.XPATH, '//div[contains(text(),"Далее")]'))).click()
         wd.find_element(By.XPATH, '//button[contains(text(),"Добавить ученика")]').click()
-        wdw(wd, 1).until(ec.presence_of_element_located(
-            (By.XPATH, "//div[contains(@class, 'styles__Row')]/div/div[2]/input"))).send_keys('Иванов')
-        wd.find_element(By.XPATH, "//div[contains(@class, 'styles__Row')][last()]/div/div[3]/input").send_keys('Костя')
+        #name = wdw(wd, 1).until(ec.presence_of_element_located(
+        #    (By.XPATH, "//div[contains(@class, 'styles__Row')]/div/div[2]/input")))
+        #def generate_random_string(8):
+        #    letters_and_digits = string.ascii_letters + string.digits
+        #    name = ''.join(random.sample(letters_and_digits, length))
+
+        # wdw(wd, 1).until(ec.presence_of_element_located(
+        #    (By.XPATH, "//div[contains(@class, 'styles__Row')]/div/div[2]/input"))).send_keys('Иванов')
+        # wd.find_element(By.XPATH, "//div[contains(@class, 'styles__Row')][last()]/div/div[3]/input").send_keys('Костя')
         wd.find_element(By.XPATH, "//div[contains(@class, 'styles__Row')][last()]/div/div[4]/label[1]").click()
         wd.find_element(By.XPATH, "//div[contains(@class, 'styles__Row')][last()]/div/div[10]/button").click()
+        # Следующий тест проверяет что есть кнопка Удалить, которая появляется только при наличии ученика
 
-    ''''def test_04_student_deleting(self):
+    def test_04_deleting_student(self):
         wd = self.wd
-
-        wd.find_element(By.XPATH, '//div[contains(text(),"Удалить")][1]').click()
-        wd.switch_to.alert.accept()'''
+        # ждём 3 секунды для того чтобы появился только что добавленный ученик
+        time.sleep(3)
+        # По кнопке Изменить смотрим количество учеников. Количество "Изменить" = количеству учеников
+        a = len(wd.find_elements(By.XPATH, '//div[contains(text(),"Изменить")]'))
+        # print(str(a) + " учеников")
+        wd.find_element(By.XPATH, '//div[contains(text(),"Удалить")]').click()
+        wd.switch_to.alert.accept()
+        # Снова считаем сколько стало Изменить
+        time.sleep(2)
+        b = len(wd.find_elements(By.XPATH, '//div[contains(text(),"Изменить")]'))
+        self.assertTrue(a > b, 'Студент не удалился')
 
 
     @classmethod
