4dad7eb45a7f36dd59fc03a3fd4295160945f2ac
diminoend <diminuend@mail.ru>
2022-05-07 23:41:35 +0300

    4 урок

---
+++ stepik/2.3 step 6.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.by import By
+import math
+
+link = 'http://suninjuly.github.io/redirect_accept.html'
+br = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+try:
+    br.get(link)
+    br.find_element(By.CLASS_NAME, "btn").click()
+    new_window = br.window_handles[1]
+    br.switch_to.window(new_window)
+    x = br.find_element(By.ID, 'input_value').text
+    y = calc(x)
+
+    br.find_element(By.CLASS_NAME, 'form-control').send_keys(y)
+    br.find_element(By.CLASS_NAME, "btn").click()
+
+finally:
+    time.sleep(5)
+    br.quit()
+
+
---
+++ stepik/2.4 step 8 .py
@@ -0,0 +1,30 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as WDW
+from webdriver_manager.chrome import ChromeDriverManager as CDM
+from selenium.webdriver.support import expected_conditions as ec
+from selenium import webdriver
+import math
+
+link = 'http://suninjuly.github.io/explicit_wait2.html'
+br = webdriver.Chrome(executable_path=CDM().install())
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+try:
+    br.get(link)
+    wait = WDW(br, 15).until(ec.text_to_be_present_in_element((By.ID, 'price'), '$100'))
+    btn_Book = br.find_element(By.CSS_SELECTOR, "[onclick='checkPrice();']").click()
+
+    x = br.find_element(By.ID, 'input_value').text
+    y = calc(x)
+
+    br.find_element(By.ID, 'answer').send_keys(y)
+    br.find_element(By.ID, 'solve').click()
+
+
+finally:
+    time.sleep(5)
+    br.quit()
